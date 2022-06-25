620ee96438992d3c1a1ca2e360c75e753016e436
trex22 <contact@jasonchalom.com>
2022-06-26 00:34:58 +0200

    Fill in services :construction_worker:

--- lib/ruby-updater/bundler_service.rb
+++ lib/ruby-updater/bundler_service.rb
@@ -2,5 +2,100 @@ module RubyUpdater
   module BundlerService
     extend self
 
+    THREE_DIGIT_VALUE = /\d+.\d+.\d+/
+    TWO_DIGIT_VALUE = /\d+.\d+/
+
+    def not_present?
+      !present?
+    end
+
+    def present?
+      `bundle --version`.include?('Bundler version')
+    end
+
+    def gemspec_present?(folder_path)
+      `cd #{folder_path} && ls |grep .gemspec`.include?('.gemspec')
+    end
+
+    def gemfile_present?(folder_path)
+      `cd #{folder_path} && ls |grep Gemfile`.include?('Gemfile')
+    end
+
+    def current_gemfile_gems_with_specified_versions(folder_path)
+      if gemspec_present?(folder_path)
+        filename = `cd #{folder_path} && ls |grep *.gemspec`.split("\n").first
+        return if filename == ''
+
+        lines = File.read("#{folder_path}/#{filename}").split("\n")
+        process_gemspec(lines)
+      elsif gemfile_present?(folder_path)
+        lines = File.read("#{folder_path}/Gemfile").split("\n")
+        process_gemfile(lines)
+      end
+    end
+
+    def remove_lockfile(folder_path)
+      `cd #{folder_path} && rm Gemfile.lock`
+    end
+
+    def install(folder_path)
+      `cd #{folder_path} && bundle install`
+    end
+
+    def update(folder_path)
+      `cd #{folder_path} && bundle update`
+    end
+
+    private
+
+    def process_gemspec(lines)
+      lines
+        .map(&:strip)
+        .reject { |line| line[0] == '#' || line.include?('git:') }
+        .select { |line| line.include?('spec.add_dependency') || line.include?('spec.add_development_dependency') }
+        .map { |line| line.gsub('spec.add_dependency ', '') }
+        .map { |line| line.gsub('spec.add_development_dependency ', '') }
+        .map { |line|
+          split_line = line.split("', '")
+          name = split_line[0].gsub("'", '')
+
+          version = split_line[1].to_s
+
+          if split_line.size > 2
+            version = split_line[1..split_line.size].join(' ')
+          end
+
+          version = version.match(THREE_DIGIT_VALUE).to_s
+          version = version.match(TWO_DIGIT_VALUE).to_s if version == ''
+
+          next if version == ''
+
+          [name, version]
+        }.compact
+    end
+
+    def process_gemfile(lines)
+      lines
+        .map(&:strip)
+        .reject { |line| line[0] == '#' || line.include?('git:') }
+        .select { |line| line.include?("gem '") }
+        .map { |line|
+          split_line = line.gsub("gem '", '').split("', '")
+          name = split_line[0].gsub("'", '')
+
+          version = split_line[1].to_s
+
+          if split_line.size > 2
+            version = split_line[1..split_line.size].join(' ')
+          end
+
+          version = version.match(THREE_DIGIT_VALUE).to_s
+          version = version.match(TWO_DIGIT_VALUE).to_s if version == ''
+
+          next if version == ''
+
+          [name, version]
+        }.compact
+    end
   end
 end
--- lib/ruby-updater/git_service.rb
+++ lib/ruby-updater/git_service.rb
@@ -2,12 +2,33 @@ module RubyUpdater
   module GitService
     extend self
 
+    def not_present?
+      !present?
+    end
+
     def present?
       `git --version`.include?('git version')
     end
 
     def branch_name(folder_path)
       `cd #{folder_path} && git status`.split("\n").first.gsub('On branch ', '')
+    end
+
+    def changes_pending?(folder_path)
+      !nothing_to_commit?(folder_path)
+    end
+
+    def nothing_to_commit?(folder_path)
+      `cd #{folder_path} && git status`.include?('nothing to commit, working tree clean')
+    end
+
+    # This can be dangerous. Check #changes_pending? before making
+    # destructive changes or roll-backs
+    def reset_changes(folder_path)
+      return if not_present?
+      return unless changes_pending?(folder_path)
+
+      `cd #{folder_path} && git stash && git stash clear`
     end
   end
 end
